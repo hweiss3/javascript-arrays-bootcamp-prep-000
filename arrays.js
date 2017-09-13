ece216db2e9c899d3327b5202c20998dee20b410
Harrison Weiss <weiss.harrison@gmail.com>
2017-09-13 16:16:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(){
   ["milkyway", ...chocolateBars]
-  return choclateBars
+  return chocolateBars
 }
